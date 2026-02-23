7c7876d168c219b652254f8a91de9307e80c5b95
Kaushal Desai <kaushaldesai906@gmail.com>
2026-02-23 20:00:05 +0530

    Delete EduNabha directory

--- EduNabha/backend/start-backend.ps1
+++
@@ -1,9 +0,0 @@
-# PowerShell script to start the EduNabha backend server
-Write-Host "Starting EduNabha Backend Server..."
-Write-Host "Press Ctrl+C to stop the server"
-
-# Navigate to the backend directory
-Set-Location $PSScriptRoot
-
-# Start the Node.js server
-node server.js
